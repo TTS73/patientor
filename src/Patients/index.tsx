d5c3110317dcc5112fc2875d14e56fea5603d7e8
Tero Salomaki <tero.salomaki@vincit.fi>
2021-02-10 22:22:11 +0200

    Task 9.20

--- src/Patients/index.tsx
+++ src/Patients/index.tsx
@@ -5,7 +5,7 @@ import { Icon } from 'semantic-ui-react';
 
 import { useStateValue } from "../state";
 import { apiBaseUrl } from "../constants";
-import { Patient } from './../types';
+import { Patient, Entry } from './../types';
 import { addSinglePatient } from './../state/reducer';
 
 
@@ -33,6 +33,15 @@ const SinglePatient: React.FC = _props => {
       <h3>{patient?.name} <Icon name={gender} /></h3>
       <p>ssn: {patient?.ssn}</p>
       <p>occupation: {patient?.occupation}</p>
+      <h4>Entries</h4>
+      {patient?.entries.map((e: Entry) => (
+        <div key={e.description}>
+          <p>{e.description}</p>
+          <ul>
+            {e.diagnosisCodes?.map(code => (<li key={code}>{code}</li>))}
+          </ul>
+        </div>
+      ))}
     </React.Fragment>
   );
 };
--- src/types.ts
+++ src/types.ts
@@ -17,4 +17,38 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Entry[];
 }
+
+
+interface BaseEntry {
+  id: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: string[];
+  description: string;
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export interface OccupationalHealthCareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: number;
+}
+
+export type Entry = HospitalEntry | OccupationalHealthCareEntry | HealthCheckEntry;
